1d8b539608e14c81e24f8d3c64fc90ce717e0007
Fabian Amino <aminofab@gmail.com>
2025-01-23 21:48:39 +0300

    added

--- lib/db.ts
+++ lib/db.ts
@@ -1,16 +1,5 @@
-import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from '@prisma/client/edge'
 
-declare global {
-  var prisma: ReturnType<typeof prismadb> | undefined
-}
+const prismadb = new PrismaClient()
 
-const prismadb = () => {
-  return new PrismaClient().$extends(withAccelerate())
-}
-
-const db = globalThis.prisma || prismadb()
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db
-
-export default db
+export default prismadb
