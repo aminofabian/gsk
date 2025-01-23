127ee9845dfea881a83f9db9459e3572fc2dc2bc
Fabian Amino <aminofab@gmail.com>
2025-01-23 22:28:50 +0300

    fix: update prisma client for edge runtime

--- lib/db.ts
+++ lib/db.ts
@@ -1,21 +1,16 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
 import { Pool } from '@neondatabase/serverless'
 
-declare global {
-  var prisma: PrismaClient | undefined
-}
-
 const connectionString = process.env.DIRECT_URL!
+
 const pool = new Pool({ connectionString })
 
-const prismadb = globalThis.prisma || new PrismaClient({
+const prismadb = new PrismaClient({
   datasources: {
     db: {
       url: connectionString
     }
   }
 })
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb
 
 export default prismadb
